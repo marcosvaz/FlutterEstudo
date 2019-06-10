f36e62cce29c3c68892ac465b5287214f299aa12
Marcos Vaz <marcosvazdeveloper@gmail.com>
2019-06-10 17:02:49 -0300

    Added some itens to layout

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,8 @@ void main() => runApp(
   ),
 );
 
+
+// Splash Screen
 class SplashScreen extends StatefulWidget {
   @override
   _SplashScreenState createState() => _SplashScreenState();
@@ -26,7 +28,6 @@ class _SplashScreenState extends State<SplashScreen> {
       () => Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (BuildContext context) => Main())),
     );
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,7 +43,7 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 }
 
-// Route Main
+// Main
 class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -51,12 +52,23 @@ class Main extends StatelessWidget {
         decoration: BoxDecoration(
           color: Color(0xFF7B139A),
         ),
-        child: Top(),
+        child: Column(
+          children: <Widget>[
+            Top(),
+            Icon(
+              Icons.keyboard_arrow_down,
+              color: Colors.white30,
+              size: 30
+            ),
+            Card(),
+          ],
+        ),
       ),
     );
   }
 }
 
+// TOP
 class Top extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -75,11 +87,85 @@ class Top extends StatelessWidget {
                 color: Color(0xFFFFFFFF),
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
-              )
-            )
+              ),
+            ),
           ),
         ],
-      )
+      ),
+    );
+  }
+}
+
+// CARD
+class Card extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Padding(
+        padding: EdgeInsets.only(top: 60),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Color(0xFFFFFFFF),
+            borderRadius: new BorderRadius.all(Radius.circular(2)),
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                color: Colors.black12,
+                offset: Offset(1, 2),
+                blurRadius: 1
+              ),
+            ],
+          ),
+          width: 325,
+          height: 370,
+          child: ListView(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(right: 10, left: 10),
+                child: Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(left: 20),
+                      child: Icon(
+                        Icons.account_balance,
+                        color: Colors.grey[400],
+                        size: 30
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 200),
+                      child: Icon(
+                        Icons.visibility_off,
+                        color: Colors.grey[400],
+                        size: 30
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 30, top: 60),
+                child: Text("Saldo disponível", style: TextStyle(color: Colors.black54)),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 30),
+                child: Text("R\$ 0,00", style: TextStyle(color: Colors.black, fontSize: 30)),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 90),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[100],
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 50, top: 30, bottom: 90),
+                    child: Text("Dinheiro guardado no valor \nde R\$ 150,00 sexta"),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
